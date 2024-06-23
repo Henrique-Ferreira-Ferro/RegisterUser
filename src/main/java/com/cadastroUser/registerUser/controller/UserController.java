45f86b439b378af2ba526813bc35ea6a089ef417
Henrique Ferreira Ferro <henriqueferreiraferro@gmail.com>
2024-06-23 10:43:29 -0300

    Criação do CRUD no service e criação dos END-POINTS no controller

--- src/main/java/com/cadastroUser/registerUser/controller/UserController.java
+++ src/main/java/com/cadastroUser/registerUser/controller/UserController.java
@@ -1,9 +1,19 @@
 package com.cadastroUser.registerUser.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cadastroUser.registerUser.entities.UserEntity;
 import com.cadastroUser.registerUser.services.UserService;
 
 @RestController
@@ -12,7 +22,32 @@ public class UserController {
 	
 	@Autowired
 	private UserService service;
+		
+	@PostMapping("/create")
+	public UserEntity createUser(UserEntity user) {
+		return service.createUser(user);
+	}
+	
+	@GetMapping("/{id}")
+	public Optional<UserEntity> findUserById(@PathVariable Long id){
+		return service.findUserById(id);
+	}
+	
+	@GetMapping
+	public List<UserEntity> findAllUsers(){
+		return service.findAllUsers();
+	}
+	
+	@PutMapping("/update/{id}")
+	public UserEntity updateUserById(@RequestBody UserEntity user, @PathVariable Long id){
+		return service.updateUser(user, id);
+	}
 	
 	
+	@DeleteMapping("/{id}")
+	public String deleteUser(@PathVariable Long id) {
+		return service.deleteUser(id);
+	}
 	
+
 }
